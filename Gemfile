886ae1fc8e25236b9088ebbcca0859bda41f3fa9
Derek Eder <derek.eder@gmail.com>
2012-10-29 17:52:29 -0500

    setup jekyll blog framework

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem "sinatra-r18n"
 gem "haml"
 
 group :development do
+  gem "rdiscount"
+  gem "jekyll"
   gem "heroku"
   gem "unicorn"
 end
--- lib/site_template/application.rb
+++ lib/site_template/application.rb
@@ -58,6 +58,32 @@ module SiteTemplate
     get "/maps/:map/" do
       redirect "/maps/#{params[:map]}/index.html"
     end
+
+    get "/blog/?*" do
+      jekyll_blog(request.path) {404}
+    end
+
+    def jekyll_blog(path, &missing_file_block)
+      @current_menu = "blog"
+      @title = "Blog - Derek Eder"
+
+      file_path = File.join(File.dirname(__FILE__), 'blog/_site',  path.gsub('/blog',''))
+      file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i  
+      puts file_path
+      if File.exist?(file_path)
+        file = File.open(file_path, "rb")
+        contents = file.read
+        file.close
+
+        if file_path.include? '.xml'
+          erb contents, :content_type => 'text/xml'
+        else
+          erb contents, :layout_engine => :haml
+        end
+      else
+        haml :not_found
+      end
+    end
     
     # catchall for static pages
     get "/:page/?" do
